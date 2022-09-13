24e14a9d8cb5794321d29208bbfbd9db89807f9e
portion-jack <flycast0080@gmail.com>
2022-09-13 14:57:14 +0900

    square2polygon

--- statistic_programming/Think_Python_2e/chap_4/mypolygon.py
+++ statistic_programming/Think_Python_2e/chap_4/mypolygon.py
@@ -1,17 +1,17 @@
 from swampy.TurtleWorld import *
 
+input_len=int(input('length : '))
+input_n = int(input('n : '))
 
 world = TurtleWorld()
 
-def square(t):
-
+def polygon(t,length=input_len,n=input_n):
     print(t)
-    for i in range(4):
-        fd(t, 100)
-        lt(t)
+    for i in range(n):
+        fd(t,length)
+        lt(t,360/n)
     return None
 
-
-square(Turtle())
+polygon(t=Turtle(),length=input_len,n=input_n)
 
 wait_for_user()
