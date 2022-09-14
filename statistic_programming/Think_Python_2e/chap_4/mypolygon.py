bbb595b4701e8915fb5ba6ce66e39ee210bfe42c
portion-jack <flycast0080@gmail.com>
2022-09-14 13:02:08 +0900

    polygon -> circle

--- statistic_programming/Think_Python_2e/chap_4/mypolygon.py
+++ statistic_programming/Think_Python_2e/chap_4/mypolygon.py
@@ -1,17 +1,41 @@
 from swampy.TurtleWorld import *
 
 input_len=int(input('length : '))
-input_n = int(input('n : '))
+# input_n = int(input('n : '))
+input_angle = float(input('angle : '))
+
 
 world = TurtleWorld()
 
-def polygon(t,length=input_len,n=input_n):
+#def polygon(t,length=input_len,n=input_n):
+#    print(t)
+#    for i in range(n):
+#        fd(t,length)
+#        lt(t,360/n)
+#    return None
+
+# polygon(t=Turtle(),length=input_len,n=input_n)
+
+def circle(t,radius=input_len):
     print(t)
-    for i in range(n):
-        fd(t,length)
-        lt(t,360/n)
+    t.delay = 0.01
+    for i in range(50):
+        fd(t,radius)
+        lt(t,360/50)
     return None
 
-polygon(t=Turtle(),length=input_len,n=input_n)
+
+def arc(t,radius,angle):
+    print(t)
+    t.delay = 0.01
+    for i in range(50):
+        fd(t,radius)
+        lt(t,angle/50)
+    return None
+
+
+arc(t=Turtle(),radius=input_len,angle=input_angle)
+
+
 
 wait_for_user()
